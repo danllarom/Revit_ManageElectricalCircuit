966ab709ff29bf431f38c40e5ded25844334b07d
unknown <daniel.llaca90@gmail.com>
2020-03-25 21:26:55 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Electrical;
+
+using System.Windows.Forms;
+using System.Windows.Media.Imaging; //https://archi-lab.net/create-your-own-tab-and-buttons-in-revit/
+using System.Diagnostics;
+
+//MATRIZ DE INCIDENCIA:
+//Cada columna representa una edges del grafo y cada fila un nodo
+//los elementos que la componen son ceros y unos: uno si la edges y el nodo correspondientes tienen relacion directa y cero si no.
+//MATRIZ DE ADYACENCIA:
+
+//Es una matriz cuadrada donde tanto las filas con mo las columnas representan todos los Aristas del grafo.
+//Los elementos que la componen son ceros y unos: uno si el nodo de la fila esta conectado por medio de una edges al modo de la columna y cero si no.
+//si la cominicacion es bidireccional la matriz es simetrica.
+
+//SECUENCIA DE GRADOS:
+//es un vector en el que cada elemento representa el numero de aristas que llegan o salen de un vertice.
+
+//Algoritmo de Floyd-Warshall
+//https://es.wikipedia.org/wiki/Algoritmo_de_Floyd-Warshall
+//https://www.programmingalgorithms.com/algorithm/floyd%E2%80%93warshall-algorithm/
+//https://www.csharpstar.com/floyd-warshall-algorithm-csharp/
+
+
+//OTHER
+//https://thebuildingcoder.typepad.com/blog/2010/07/retrieve-structural-elements.html
+
+namespace Revit_ManageElectricalCircuit
+{
+    public struct nodo
+    {
+        public nodo(int name, XYZ location, Connector conector)
+        {
+            Name = name;
+            Location = location;
+            Conector = conector;
+        }
+
+        public int Name { get; set; }
+        public XYZ Location { get; set; }
+        public Connector Conector { get; set; }
+    }
+    public struct edges
+    {
+        public edges(int nodeA, int nodeB, double lenth)
+        {
+            NodeA = nodeA;
+            NodeB = nodeB;
+            Lenth = lenth;
+        }
+
+        public int NodeA { get; set; }
+        public int NodeB { get; set; }
+        public double Lenth { get; set; }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            try
+            {
+                //get all cable tray element in de model
+                FilteredElementCollector collectorCableTrayFitting = new FilteredElementCollector(doc);
+                collectorCableTrayFitting = GetConnectorElements(doc, false);
+                
+                FloydWarshall floydWarshall = new FloydWarshall();
+                floydWarshall.PlayFloydWarshall(GetLines(collectorCableTrayFitting));
+
+                MessageBox.Show(floydWarshall.nodos.Count().ToString());
+
+                //Pick Circuit
+                FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                collector1.OfCategory(BuiltInCategory.OST_ElectricalCircuit);
+
+                nodo nodeA = new nodo();
+                nodo nodeB = new nodo();
+
+                foreach (ElectricalSystem circuito in collector1)
+                {
+                    LocationPoint locationPanel = circuito.BaseEquipment.Location as LocationPoint;
+                    XYZ XYZPanel = locationPanel.Point;
+
+                    int j = 0;
+                    foreach (nodo node in floydWarshall.nodos)
+                    {
+                        if (j == 0)
+                        {
+                            nodeA.Location = node.Location;
+                            nodeA.Name = node.Name;
+                            j++;
+                        }
+                        else if (Math.Abs(XYZPanel.Subtract(node.Location).GetLength()) < Math.Abs(XYZPanel.Subtract(nodeA.Location).GetLength()))
+                        {
+                            nodeA.Location = node.Location;
+                            nodeA.Name = node.Name;
+                            j++;
+                        }
+                    }
+                    j = 0;
+                    List<XYZ> CircuitsElement = new List<XYZ>();
+                    foreach (Element elem in circuito.Elements)
+                    {
+                        FamilyInstance elemCircuit = elem as FamilyInstance;
+                        LocationPoint locationelem = elemCircuit.Location as LocationPoint;
+                        XYZ XYZelem = locationelem.Point;
+                        CircuitsElement.Add(XYZelem);
+
+                        foreach (nodo node in floydWarshall.nodos)
+                        {
+                            if (j == 0)
+                            {
+                                nodeB.Location = node.Location;
+                                nodeB.Name = node.Name;
+                                j++;
+                            }
+                            else if (Math.Abs(XYZelem.Subtract(node.Location).GetLength()) < Math.Abs(XYZelem.Subtract(nodeB.Location).GetLength()))
+                            {
+                                nodeB.Location = node.Location;
+                                nodeB.Name = node.Name;
+                                j++;
+                            }
+                        }
+                    }
+                    floydWarshall.GetPath(nodeA.Name, nodeB.Name);
+
+                    Transaction trans2 = new Transaction(doc);
+                    trans2.Start("Lab");
+                    circuito.SetCircuitPath(floydWarshall.organizePath(nodeA.Name, nodeB.Name, XYZPanel, CircuitsElement));
+                    trans2.Commit();
+                }
+            }
+            //If the user right-clicks or presses Esc, handle the exception
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            //Catch other errors
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+        public class ElectricalCircuitPickFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element e)
+            {
+                return (e.Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_ElectricalCircuit));
+            }
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return false;
+            }
+        }
+        static FilteredElementCollector GetConnectorElements(Document doc, bool include_wires)
+        {
+            //https://thebuildingcoder.typepad.com/blog/2010/06/retrieve-mep-elements-and-connectors.html
+            // what categories of family instances
+            // are we interested in?
+
+            BuiltInCategory[] bics = new BuiltInCategory[] {
+                //BuiltInCategory.OST_CableTray,
+                BuiltInCategory.OST_CableTrayFitting,
+                //BuiltInCategory.OST_Conduit,
+                //BuiltInCategory.OST_ConduitFitting,
+                //BuiltInCategory.OST_DuctCurves,
+                //BuiltInCategory.OST_DuctFitting,
+                //BuiltInCategory.OST_DuctTerminal,
+                //BuiltInCategory.OST_ElectricalEquipment,
+                //BuiltInCategory.OST_ElectricalFixtures,
+                //BuiltInCategory.OST_LightingDevices,
+                //BuiltInCategory.OST_LightingFixtures,
+                //BuiltInCategory.OST_MechanicalEquipment,
+                //BuiltInCategory.OST_PipeCurves,
+                //BuiltInCategory.OST_PipeFitting,
+                //BuiltInCategory.OST_PlumbingFixtures,
+                //BuiltInCategory.OST_SpecialityEquipment,
+                //BuiltInCategory.OST_Sprinklers,
+                //BuiltInCategory.OST_Wire,
+            };
+
+            IList<ElementFilter> a
+              = new List<ElementFilter>(bics.Count());
+
+            foreach (BuiltInCategory bic in bics)
+            {
+                a.Add(new ElementCategoryFilter(bic));
+            }
+
+            LogicalOrFilter categoryFilter
+              = new LogicalOrFilter(a);
+
+            LogicalAndFilter familyInstanceFilter
+              = new LogicalAndFilter(categoryFilter,
+                new ElementClassFilter(
+                  typeof(FamilyInstance)));
+
+            IList<ElementFilter> b
+              = new List<ElementFilter>(6);
+
+            b.Add(new ElementClassFilter(typeof(CableTray)));
+            //b.Add(new ElementClassFilter(typeof(Conduit)));
+            //b.Add(new ElementClassFilter(typeof(Duct)));
+            //b.Add(new ElementClassFilter(typeof(Pipe)));
+
+            if (include_wires)
+            {
+                b.Add(new ElementClassFilter(typeof(Wire)));
+            }
+
+            b.Add(familyInstanceFilter);
+
+            LogicalOrFilter classFilter
+              = new LogicalOrFilter(b);
+
+            FilteredElementCollector collector
+              = new FilteredElementCollector(doc);
+
+            collector.WherePasses(classFilter);
+
+            return collector;
+        }
+        static ConnectorSet GetConnectors(Element e)
+        {
+            ConnectorSet connectors = null;
+
+            if (e is FamilyInstance)
+            {
+                MEPModel m = ((FamilyInstance)e).MEPModel;
+
+                if (null != m
+                  && null != m.ConnectorManager)
+                {
+                    connectors = m.ConnectorManager.Connectors;
+                }
+            }
+            else if (e is Wire)
+            {
+                connectors = ((Wire)e)
+                  .ConnectorManager.Connectors;
+            }
+            else
+            {
+                Debug.Assert(e.GetType().IsSubclassOf(typeof(MEPCurve)),"expected all candidate connector provider " 
+                    + "elements to be either family instances or "
+                    + "derived from MEPCurve");
+
+                if (e is MEPCurve)
+                {
+                    connectors = ((MEPCurve)e)
+                      .ConnectorManager.Connectors;
+                }
+            }
+            return connectors;
+        }
+        static List<XYZ[]> GetLines(FilteredElementCollector collectorCableTrayFitting)
+        {
+            List<XYZ[]> Aristas = new List<XYZ[]>();
+            List<nodo> connectorsNodo = new List<nodo>();
+            int i = 0;
+            foreach (Element elem in collectorCableTrayFitting)
+            {
+                ConnectorSet conectores = GetConnectors(elem);
+
+                i = 0;
+
+                if (conectores.Size == 3)
+                {
+                    XYZ[] P = new XYZ[3];
+                    XYZ Pcentro = null;
+                    double[] L = new double[3];
+
+                    foreach (Connector conector in conectores)
+                    {
+                        P[i] = conector.Origin;
+                        i++;
+                        var testWasTrue = false;
+                        foreach (nodo connector1 in connectorsNodo)
+                        {
+                            if (conector.IsConnectedTo(connector1.Conector))
+                            {
+                                testWasTrue = true;
+                                break;
+                            }
+                        }
+                        if (testWasTrue == false)
+                        {
+                            connectorsNodo.Add(new nodo(0, null, conector));
+                        }
+                    }
+                    L[0] = P[0].Subtract(P[1]).GetLength();
+                    L[0] = Math.Abs(L[0]);
+                    L[1] = P[1].Subtract(P[2]).GetLength();
+                    L[1] = Math.Abs(L[1]);
+                    L[2] = P[2].Subtract(P[0]).GetLength();
+                    L[2] = Math.Abs(L[2]);
+
+                    double Lmax = Math.Max(L[0], Math.Max(L[1], L[2]));
+
+                    if (Lmax == L[0])
+                    {
+                        Pcentro = P[0].Subtract(P[1]).Divide(2).Add(P[1]);
+                    }
+                    if (Lmax == L[1])
+                    {
+                        Pcentro = P[1].Subtract(P[2]).Divide(2).Add(P[2]);
+                    }
+                    if (Lmax == L[2])
+                    {
+                        Pcentro = P[2].Subtract(P[0]).Divide(2).Add(P[0]);
+                    }
+                    foreach (Connector conector in conectores)
+                    {
+                        XYZ[] P1 = new XYZ[2];
+                        P1[0] = conector.Origin;
+                        P1[1] = Pcentro;
+                        Aristas.Add(P1);
+                        i++;
+                    }
+
+                }
+                if (conectores.Size == 4)
+                {
+                    XYZ[] P = new XYZ[4];
+                    foreach (Connector conector in conectores)
+                    {
+                        P[i] = conector.Origin;
+                        i++;
+                        var testWasTrue = false;
+                        foreach (nodo connector1 in connectorsNodo)
+                        {
+                            if (conector.IsConnectedTo(connector1.Conector))
+                            {
+                                testWasTrue = true;
+                                break;
+                            }
+                        }
+                        if (testWasTrue == false)
+                        {
+                            connectorsNodo.Add(new nodo(0, null, conector));
+                        }
+                    }
+                    XYZ Pcentro = P[0].Add(P[1]).Add(P[2]).Add(P[3]).Divide(4);
+                    i = 0;
+                    foreach (Connector conector in conectores)
+                    {
+                        XYZ[] P1 = new XYZ[2];
+                        P1[0] = conector.Origin;
+                        P1[1] = Pcentro;
+                        Aristas.Add(P1);
+                        i++;
+                    }
+                }
+                if (conectores.Size == 2)
+                {
+                    XYZ[] P = new XYZ[2];
+                    foreach (Connector conector in conectores)
+                    {
+                        P[i] = conector.Origin;
+                        i++;
+
+                        var testWasTrue = false;
+                        foreach (nodo connector1 in connectorsNodo)
+                        {
+                            if (conector.IsConnectedTo(connector1.Conector))
+                            {
+                                testWasTrue = true;
+                                break;
+                            }
+                        }
+                        if (testWasTrue == false)
+                        {
+                            connectorsNodo.Add(new nodo(0, null, conector));
+                        }
+                    }
+                    Aristas.Add(P);
+                }
+            }
+
+            
+            return Aristas;
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Electrical;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Revit_ManageElectricalCircuit
+{
+    class FloydWarshall
+    {
+        public double[,] AdjacencyMatrix=null;
+        public double[,] AdjacencyMatrix0 = null;
+        public int[,] PathMatrix = null;
+        public List<nodo> nodos = new List<nodo> { };
+        public List<XYZ[]> Aristas = null;
+        public List<edges> Edges = new List<edges> { };
+        List<int> path = new List<int>();
+
+        public FloydWarshall()
+        {
+        }
+        public List<nodo> GetNodos()
+        {
+            int i = 0;
+            foreach (XYZ[] arista in Aristas)
+            {
+                var testWasTrue = false;
+
+                if (nodos.Count() == 0)
+                {
+                    nodo punto = new nodo(i, arista[0], null);
+                    nodos.Add(punto);
+                    i++;
+                }
+                foreach (nodo nodo1 in nodos)
+                {
+                    if (Math.Abs(arista[0].DistanceTo(nodo1.Location))<=1e-9)
+                    {
+                        testWasTrue = true;
+                        break;
+                    }
+                }
+
+                if (testWasTrue==false)
+                {
+                    nodo punto = new nodo(i, arista[0],null);
+                    nodos.Add(punto);
+                    i++;
+                }
+                testWasTrue = false;
+                foreach (nodo nodo1 in nodos)
+                {
+                    if (Math.Abs(arista[1].DistanceTo(nodo1.Location)) <=1e-9)
+                    {
+                        testWasTrue = true;
+                        break;
+                    }
+                }
+                if (testWasTrue==false)
+                {
+                    nodo punto = new nodo(i, arista[1],null);
+                    nodos.Add(punto);
+                    i++;
+                }
+            }
+            return nodos;
+        }
+        public List<edges> GetEdges()
+        {
+            Edges = new List<edges> { };
+            foreach (XYZ[] arista in Aristas)
+            {
+                edges line1 = new edges{Lenth = Math.Abs(arista[0].Subtract(arista[1]).GetLength())};
+
+                foreach (nodo nodo1 in nodos)
+                {
+                    if (Math.Abs(arista[0].Subtract(nodo1.Location).GetLength()) <= 1e-9) 
+                    {
+                        line1.NodeA = nodo1.Name;
+                    }
+                    if (Math.Abs(arista[1].Subtract(nodo1.Location).GetLength()) <= 1e-9)
+                    {
+                        line1.NodeB = nodo1.Name;
+                    }
+                }
+                Edges.Add(line1);
+            }
+            return Edges;
+        }
+        public double[,] GetAdjacencyMatrix()
+        {
+            int Nnodes = nodos.Count();
+
+            AdjacencyMatrix = new double[Nnodes, Nnodes];
+            AdjacencyMatrix0 = new double[Nnodes, Nnodes];
+
+            for (int i = 0; i < Nnodes; ++i)
+            {
+                for (int j = 0; j < Nnodes; ++j)
+                {
+                    AdjacencyMatrix[i, j] = PositiveInfinity;
+                    AdjacencyMatrix0[i, j] = 0;
+                    if (i == j)
+                    {
+                        AdjacencyMatrix[i, j] = 0;
+                    }
+                }
+            }
+            foreach (edges arista1 in Edges)
+            {
+                AdjacencyMatrix[arista1.NodeA, arista1.NodeB] = arista1.Lenth;
+                AdjacencyMatrix[arista1.NodeB, arista1.NodeA] = arista1.Lenth;
+                AdjacencyMatrix0[arista1.NodeA, arista1.NodeB] = 1;
+                AdjacencyMatrix0[arista1.NodeB, arista1.NodeA] = 1;
+            }
+            return AdjacencyMatrix;
+        }
+        public const double PositiveInfinity = 1.5e300;
+        public int[,] PlayFloydWarshall(List<XYZ[]> aristas)
+        {
+            Aristas = new List<XYZ[]> (aristas);
+
+            nodos = new List<nodo>(GetNodos());
+
+            Edges = new List<edges>(GetEdges());
+
+            AdjacencyMatrix = (double[,])GetAdjacencyMatrix().Clone();
+
+            //PathMatrix
+            //     1-2-3-4=j
+            //i=1-(1,2,3,4)
+            //i=2-(1,2,3,4)
+            //i=3-(1,2,3,4)
+            //i=4-(1,2,3,4)
+            int Nnodes = nodos.Count();
+            PathMatrix = new int[Nnodes, Nnodes];
+            
+            for (int i = 0; i < Nnodes; ++i)
+            {
+                for (int j = 0; j < Nnodes; ++j)
+                {
+                    if (AdjacencyMatrix[i, j] != PositiveInfinity)
+                    {
+                        PathMatrix[i, j] = i;
+                    }
+                    else
+                    {
+                        PathMatrix[i, j] = -1;
+                    }
+                    if (i == j)
+                    {
+                        PathMatrix[i, j] = i;
+                    }
+                }
+            }
+            
+            //FloydWarshall
+            for (int k = 0; k < Nnodes; ++k)
+            {
+                for (int i = 0; i < Nnodes; ++i)
+                {
+                    for (int j = 0; j < Nnodes; ++j)
+                    {
+                        if ((i != k) && (j != k) && (AdjacencyMatrix[i, k] != PositiveInfinity) && (AdjacencyMatrix[k, j] != PositiveInfinity))
+                        {
+                            if (((AdjacencyMatrix[i, k] + AdjacencyMatrix[k, j]) < AdjacencyMatrix[i, j]))
+                            {
+                                AdjacencyMatrix[i, j] = AdjacencyMatrix[i, k] + AdjacencyMatrix[k, j];
+                                PathMatrix[i, j] = k;
+                            }
+                        }
+                    }
+                }
+            }
+            return PathMatrix;
+        }
+        public List<int> GetPath(int nodeInit, int nodeEnd)
+        {
+            //http://web.mit.edu/urban_or_book/www/book/chapter6/6.2.2.html
+            //https://medium.com/@vighneshtiwari16377/floyd-warshall-dynamic-programming-algorithm-e2a899c3e5e6
+
+            path.Add(nodeInit);
+            path.Add(nodeEnd);
+
+            readPathMatrix(nodeInit, nodeEnd);
+            
+            return path;
+        }
+        public XYZ GetXYZNode(int node)
+        {
+            XYZ point = null;
+            foreach (nodo nodei in nodos)
+            {
+                if (node == nodei.Name)
+                {
+                    point = nodei.Location;
+                    break;
+                }
+            }
+            return point;
+        }
+        public void readPathMatrix(int nodeInit, int nodeEnd)
+        {
+            if (((PathMatrix[nodeInit, nodeEnd] != nodeInit) && (PathMatrix[nodeInit, nodeEnd] != nodeEnd))|| ((PathMatrix[nodeEnd, nodeInit] != nodeInit) && (PathMatrix[nodeEnd, nodeInit] != nodeEnd)))
+            {
+                int node = nodeEnd;
+                node = PathMatrix[nodeInit, node];
+                path.Add(node);
+
+                if ((PathMatrix[nodeInit, node] != nodeInit) && (PathMatrix[nodeInit, node] != node) && (node != -1))
+                {
+                    readPathMatrix(nodeInit, node);
+                }
+                else if ((PathMatrix[node, nodeInit] != nodeInit) && (PathMatrix[node, nodeInit] != node) && (node != -1))
+                {
+                    readPathMatrix(node, nodeInit);
+                }
+
+                if ((PathMatrix[nodeEnd, node] != nodeEnd) && (PathMatrix[nodeEnd, node] != node) && (node != -1))
+                {
+                    readPathMatrix(nodeEnd, node);
+                }
+                else if ((PathMatrix[node, nodeEnd] != nodeEnd) && (PathMatrix[node, nodeEnd] != node) && (node != -1))
+                {
+                    readPathMatrix(node, nodeEnd);
+                }
+            }
+        }
+        public List<XYZ> organizePath(int nodeInit, int nodeEnd, XYZ nodePanel, List<XYZ> element)
+        {
+            List<XYZ> path1 = new List<XYZ>();
+
+            if (nodePanel != null)
+            {
+                path1.Add(nodePanel);
+                if (nodePanel.Z != GetXYZNode(nodeInit).Z)
+                {
+                    XYZ a = new XYZ(nodePanel.X, nodePanel.Y, GetXYZNode(nodeInit).Z);
+                    path1.Add(a);
+                }
+            }
+            path1.Add(GetXYZNode(nodeInit));
+            path.Remove(nodeInit);
+
+            while (true)
+            {
+                //TODO: evitar el bucle infinito si i no aumenta
+                foreach (int node in path)
+                {
+                    if (AdjacencyMatrix0[nodeInit, node] == 1)
+                    {
+                        if (Math.Abs(path1.Last().Z-GetXYZNode(node).Z)>1e-9)
+                        {
+                            XYZ a = new XYZ(path1.Last().X, path1.Last().Y, GetXYZNode(node).Z);
+                            path1.Add(a);
+                        }
+                        path1.Add(GetXYZNode(node));
+                        path.Remove(node);
+                        nodeInit = node;
+                        break;
+                    }
+                }
+                if (nodeInit == nodeEnd)
+                {
+                    break;
+                }
+            }
+            foreach (XYZ elem in element)
+            {
+                if (path1.Last().Z != elem.Z)
+                {
+                    XYZ a = new XYZ(path1.Last().X, path1.Last().Y, elem.Z);
+                    path1.Add(a);
+                }
+                path1.Add(elem);
+            }
+
+            return path1;
+        }
+    }
+}
